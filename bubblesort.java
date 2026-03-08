a4379be7706db7b5b06a81e043cdbd064046b982
saidineshghodkar <saidineshghodkar@gmail.com>
2026-03-08 17:34:30 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+  
+
+          int[] arr = {5, 2, 9, 1, 5};
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+}
